99d463072d8c61a925c1c4e913b4cfe53efa7f49
RennerBS <61088717+RennerBS@users.noreply.github.com>
2020-03-24 21:14:46 -0300

    Inicio Layout

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("GLOSSAURO",
+                style: TextStyle(
+                    color: Colors.green[400],
+                    fontSize: 35.0,
+                    fontFamily: 'Slackey')),
+            centerTitle: true,
+            backgroundColor: Colors.brown),
+        body: Stack(
+          children: <Widget>[
+            Image.asset(
+              "images/Dino1.jpg",
+              fit: BoxFit.cover,
+              height: 1000.0,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                    padding: EdgeInsets.fromLTRB(20.0, 100.0, 20.0, 0.0),
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Quiz",
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.green[400],
+                            fontFamily: 'Slackey'),
+                      ),
+                      color: Colors.brown,
+                    )),
+                Container(
+                    padding: EdgeInsets.fromLTRB(20.0, 50.0, 20.0, 0.0),
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Pontuação",
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.green[400],
+                            fontFamily: 'Slackey'),
+                      ),
+                      color: Colors.brown,
+                    )),
+                Container(
+                    padding: EdgeInsets.fromLTRB(20.0, 50.0, 20.0, 0.0),
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Sobre",
+                        style: TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.green[400],
+                            fontFamily: 'Slackey'),
+                      ),
+                      color: Colors.brown,
+                    ))
+              ],
+            )
+          ],
+        ));
+  }
+}
+
+void main() {
+  runApp(new MaterialApp(home: Home()));
+}
